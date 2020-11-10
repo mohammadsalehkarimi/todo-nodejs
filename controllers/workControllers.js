e625e4f923c1567c75f46ab629b44f3e618564f0
mohammadsaleh karimi <mohammadsalehkarimi@outlook.com>
2020-11-10 15:55:35 +0330

    Done Project!

--- controllers/workControllers.js
+++ controllers/workControllers.js
@@ -1,33 +1,26 @@
 const Work = require("../modules/work");
 
-const allWork = (req, res) => {
-  Work.find()
-    .then((result) => {
-      res.render("index", { title: "ToDo", work: result });
-    })
-    .catch((err) => console.log(err));
+const allWork = (res) => {
+  Work.find().then((result) => {
+    res.render("index", { title: "ToDo", work: result });
+  });
 };
 
 const newWork = (req, res) => {
   const work = new Work(req.body);
-  work
-    .save()
-    .then((result) => {
-      res.redirect("/");
-      console.log(result);
-    })
-    .catch((err) => console.log(err));
+  work.save().then((result) => {
+    res.redirect("/");
+    console.log(result);
+  });
 };
 
 const deleteWork = (req, res) => {
   const id = req.params.id;
   console.log(id);
 
-  Work.findByIdAndDelete(id)
-    .then(() => {
-      res.json();
-    })
-    .catch((err) => console.log(err));
+  Work.findByIdAndDelete(id).then(() => {
+    res.json();
+  });
 };
 
 module.exports = {
